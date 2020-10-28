f84008a5509552687c39d58701db8b11b95f124c
david roon <david@roon.me>
2020-10-28 20:28:43 +0100

    graph

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,6 +2,14 @@ import { ProcessedProposal, SponsoredProposal, SubmittedProposal, UpdateDelegate
 import { Proposal } from '../generated/schema'
 
 export function handleSubmittedProposal(event: SubmittedProposal): void {
+  const id = event.params.proposalId;
+  let proposal = Proposal.load(id);
+  if(proposal == null) {
+      proposal = new Proposal(id);
+  }
+
+  
+  proposal.timestamp = event.block.timestamp.toString();
 }
 
 export function handleProcessedProposal(event: ProcessedProposal): void {
@@ -10,10 +18,14 @@ export function handleProcessedProposal(event: ProcessedProposal): void {
 export function handleSponsoredProposal(event: SponsoredProposal): void {
 }
 
-        
 export function  handleAdapterAdded(event: AdapterAdded): void {}
-export function  handleAdapterRemoved(event: AdapterRemoved): void {}        
-export function  handleUpdateDelegateKey(event: UpdateDelegateKey): void {}        
+
+export function  handleAdapterRemoved(event: AdapterRemoved): void {}
+
+export function  handleUpdateDelegateKey(event: UpdateDelegateKey): void {}
+
 export function  handleMemberJailed(event: MemberJailed): void {}
+
 export function  handleMemberUnjailed(event: MemberUnjailed): void {}
+
 export function  handleNewBalance(event: NewBalance): void {}
