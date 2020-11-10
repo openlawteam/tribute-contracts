0c4b361b82c989cbe09d344354f9f3dd77edc64f
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-11-10 10:46:03 -0500

    Create Address.sol
    add address library to help check non-standard erc20 in LaoLand guild banks

---
+++ contracts/utils/Address.sol
@@ -0,0 +1,37 @@
+/**
+ * @title Address
+ * @dev Collection of functions related to the address type.
+ * Forked from Paul Berg:
+ * https://raw.githubusercontent.com/PaulRBerg/contracts/main/contracts/utils/Address.sol
+ */
+library Address {
+    /**
+     * @dev Returns true if `account` is a contract.
+     *
+     * [IMPORTANT]
+     * ====
+     * It is unsafe to assume that an address for which this function returns
+     * false is an externally-owned account (EOA) and not a contract.
+     *
+     * Among others, `isContract` will return false for the following
+     * types of addresses:
+     *
+     *  - an externally-owned account
+     *  - a contract in construction
+     *  - an address where a contract will be created
+     *  - an address where a contract lived, but was destroyed
+     * ====
+     */
+    function isContract(address account) internal view returns (bool) {
+        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
+        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
+        // for accounts without code, i.e. `keccak256('')`.
+        bytes32 codehash;
+        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        /* solhint-disable-next-line no-inline-assembly */
+        assembly {
+            codehash := extcodehash(account)
+        }
+        return (codehash != accountHash && codehash != 0x0);
+    }
+}
