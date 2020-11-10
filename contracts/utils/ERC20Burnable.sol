028ff022ea508cf9e560a48fe3b44f0a2c95384a
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-11-10 10:29:26 -0500

    Create ERC20Burnable.sol
    implement token burn functions // adapted from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Burnable.sol

---
+++ contracts/utils/ERC20Burnable.sol
@@ -0,0 +1,35 @@
+/**
+ * @dev Extension of {ERC20} that allows token holders to destroy both their own
+ * tokens and those that they have an allowance for, in a way that can be
+ * recognized off-chain (via event analysis).
+ */
+abstract contract ERC20Burnable is ERC20 {
+    using SafeMath for uint256;
+    
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {ERC20-_burn}.
+     */
+    function burn(uint256 amount) public virtual {
+        _burn(msg.sender, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {ERC20-_burn} and {ERC20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
+     */
+    function burnFrom(address account, uint256 amount) public virtual {
+        uint256 decreasedAllowance = allowance(account, msg.sender).sub(amount);
+
+        _approve(account, msg.sender, decreasedAllowance);
+        _burn(account, amount);
+    }
+}
