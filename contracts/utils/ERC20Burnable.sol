41cd3d446287b785e7e65ff634da374d4322d4e6
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-11-10 10:57:30 -0500

    Update ERC20Burnable.sol
    add missing compiler

--- contracts/utils/ERC20Burnable.sol
+++ contracts/utils/ERC20Burnable.sol
@@ -1,3 +1,7 @@
+pragma solidity ^0.7.0;
+
+// SPDX-License-Identifier: MIT
+
 /**
  * @dev Extension of {ERC20} that allows token holders to destroy both their own
  * tokens and those that they have an allowance for, in a way that can be
