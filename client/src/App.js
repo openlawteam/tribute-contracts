67863e7302341ee457169bcc389851722da10a1c
Felipe Forbeck <felipe.forbeck@gmail.com>
2020-12-14 18:03:50 -0300

    init web3modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,64 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Typography } from "@material-ui/core";
 import "./App.css";
 
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+  },
+};
+
+const web3Modal = new Web3Modal({
+  network: "rinkeby", // optional
+  cacheProvider: true, // optional
+  providerOptions, // required
+});
+
 const App = () => {
-  const ethereum = window.ethereum;
-  const [addr, setAddr] = useState(null);
-  if (ethereum) {
-    ethereum.on("accountsChanged", (accounts) => setAddr(accounts[0]));
+  const [provider, setProvider] = useState();
+  const [web3, setWeb3] = useState();
+
+  useEffect(() => {
+    web3Modal
+      .connect()
+      .then((p) => {
+        setProvider(p);
+        setWeb3(new Web3(p));
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
+  const [addr, setAddr] = useState();
+  if (provider) {
+    provider.on("accountsChanged", (accounts) => {
+      console.log(accounts[0]);
+      setAddr(accounts[0]);
+    });
+    provider.on("connect", (info) => console.log(info));
+    provider.on("disconnect", (error) => console.log(error));
   }
 
+  const getBalance = async () => {
+    await web3.eth.getBalance(addr);
+  };
+
   return (
-    <div className="App">
-      {addr && <p>Connected Account: {addr} </p>}
-      {!addr && <p>Please connect to your MetaMask account</p>}
-    </div>
+    <Container className="App">
+      <Typography variant="h6">
+        {addr ? (
+          <>
+            Connected Account: {addr} - {getBalance()}
+          </>
+        ) : (
+          <>Please connect to your MetaMask account</>
+        )}
+      </Typography>
+    </Container>
   );
 };
 
