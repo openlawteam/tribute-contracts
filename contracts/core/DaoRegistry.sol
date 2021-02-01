6864dda52530f678d94c976d6aa049d2faeed70d
Felipe Forbeck <felipe.forbeck@gmail.com>
2021-02-01 14:31:07 -0300

    Fix lint

--- contracts/core/DaoRegistry.sol
+++ contracts/core/DaoRegistry.sol
@@ -490,7 +490,7 @@ contract DaoRegistry is MemberGuard, AdapterGuard {
         hasAccess(this, AclFlag.SPONSOR_PROPOSAL)
         onlyMember2(this, sponsoringMember)
     {
-        // also checks if the flag was already set 
+        // also checks if the flag was already set
         Proposal storage proposal =
             _setProposalFlag(proposalId, ProposalFlag.SPONSORED);
 
