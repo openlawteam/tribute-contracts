a7fd8a8dcd86dfe34fae27ca9ef1e8fcfdbbcc00
David Roon <david@roon.me>
2021-02-11 13:33:44 +0100

    comment out lockFunds for now (#170)
    * comment out lockFunds for now
    
    * reverted too much :)
    
    * add missing acl config for offchain voting and bank

--- contracts/adapters/voting/OffchainVoting.sol
+++ contracts/adapters/voting/OffchainVoting.sol
@@ -374,13 +374,11 @@ contract OffchainVotingContract is
         uint256 nbNo
     ) internal view returns (bool) {
         uint256 diff;
-
         if (nbYes > nbNo) {
             diff = nbYes - nbNo;
         } else {
             diff = nbNo - nbYes;
         }
-
         BankExtension bank = BankExtension(dao.getExtensionAddress(BANK));
         uint256 totalWeight = bank.getPriorAmount(TOTAL, SHARES, vote.snapshot);
         uint256 unvotedWeights = totalWeight - nbYes - nbNo;
@@ -426,7 +424,7 @@ contract OffchainVotingContract is
             "vote:sig bad"
         );
 
-        _lockFunds(dao, reporter);
+        //_lockFunds(dao, reporter);
         vote.nbNo = result.nbNo;
         vote.nbYes = result.nbYes;
         vote.index = result.index;
@@ -436,6 +434,7 @@ contract OffchainVotingContract is
         vote.gracePeriodStartingTime = block.timestamp;
     }
 
+    /*
     function _lockFunds(DaoRegistry dao, address memberAddr) internal {
         uint256 lootToLock = dao.getConfiguration(StakingAmount);
         //lock if member has enough loot
@@ -451,7 +450,6 @@ contract OffchainVotingContract is
         bank.subtractFromBalance(memberAddr, LOOT, lootToLock);
     }
 
-    /*
     function _releaseFunds(DaoRegistry dao, address memberAddr) internal {
         uint256 lootToRelease = dao.getConfiguration(StakingAmount);
         //release if member has enough locked loot
