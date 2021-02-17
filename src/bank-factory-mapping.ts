02eeb3819798ed5a90f517cecd829d9ac87d63bb
sophiacodes <sophiacodes@gmail.com>
2021-02-17 17:12:20 +0000

    updated bank factory event param

--- src/bank-factory-mapping.ts
+++ src/bank-factory-mapping.ts
@@ -18,28 +18,23 @@ function loadOrCreateBank(address: string): Bank {
  * @param event BankCreated
  * Start indexing the bank;
  * `event.params.bankAddress` is the address of the new bank contract
- * `event.params.daoAddress` is the address of the banks dao contract
  *
- *  BankExtension.create(event.params.daoAddress, event.params.bankAddress);
+ *  BankExtension.create(event.params.bankAddress);
  */
 export function handleBankCreated(event: BankCreated): void {
-  log.info(
-    "**************** handleBankCreated event fired. bankAddress: {}, daoAddress: {}",
-    [
-      event.params.bankAddress.toHexString(),
-      event.params.daoAddress.toHexString(),
-    ]
-  );
+  log.info("**************** handleBankCreated event fired. bankAddress: {}", [
+    event.params.bankAddress.toHexString(),
+  ]);
 
   let bank = loadOrCreateBank(event.params.daoAddress.toHexString());
 
   bank.createdAt = event.block.timestamp.toString();
   bank.bankAddress = event.params.bankAddress;
-  bank.daoAddress = event.params.daoAddress;
+  // bank.daoAddress = event.params.daoAddress;
   bank.balance = BigInt.fromI32(0);
 
   // create 1-to-1 relationship between the bank and its dao
-  bank.laoland = event.params.daoAddress.toHexString();
+  // bank.laoland = event.params.daoAddress.toHexString();
 
   BankExtension.create(event.params.daoAddress);
 
