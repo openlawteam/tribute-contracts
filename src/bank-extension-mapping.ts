738b87eb37271d664e42efd330e3f360c4df2cd7
sophiacodes <sophiacodes@gmail.com>
2021-03-09 13:31:21 +0000

    omit TOTAL and GUILD from the member list

--- src/bank-extension-mapping.ts
+++ src/bank-extension-mapping.ts
@@ -8,6 +8,7 @@ import { GUILD, LOCKED_LOOT, LOOT, SHARES, TOTAL } from "./helpers/constants";
 import { Address, BigInt, log } from "@graphprotocol/graph-ts";
 
 function internalTransfer(
+  createdAt: string,
   extensionAddress: Address,
   memberAddress: Address,
   tokenAddress: Address
@@ -16,68 +17,80 @@ function internalTransfer(
   let registry = BankExtension.bind(extensionAddress);
   let daoAddress = registry.dao();
 
-  let membertokenBalanceId = daoAddress
-    .toHex()
-    .concat("-tokenbalances-")
-    .concat(memberAddress.toHex());
-
-  let member = Member.load(memberAddress.toHex());
-  let token = Token.load(tokenAddress.toHex());
-  let tokenBalance = TokenBalance.load(membertokenBalanceId);
-
-  if (member == null) {
-    member = new Member(memberAddress.toHex());
-    // member.createdAt = event.block.timestamp.toString(); // @todo
-    member.memberAddress = memberAddress;
-  }
-
-  if (token == null) {
-    token = new Token(tokenAddress.toHex());
-    token.tokenAddress = tokenAddress;
-  }
-
-  if (tokenBalance == null) {
-    tokenBalance = new TokenBalance(membertokenBalanceId);
-    // we give it an initial 0 balance
-    tokenBalance.tokenBalance = BigInt.fromI32(0);
-  }
-
-  /**
-   * get `balanceOf`
-   */
-
-  // get balanceOf member shares
-  let balanceOfSHARES = registry.balanceOf(memberAddress, SHARES);
-  member.shares = balanceOfSHARES;
-
-  // get balanceOf member loot
-  let balanceOfLOOT = registry.balanceOf(memberAddress, LOOT);
-  member.loot = balanceOfLOOT;
-
-  // get balanceOf member locked loot
-  let balanceOfLOCKED_LOOT = registry.balanceOf(memberAddress, LOCKED_LOOT);
-  member.lockedLoot = balanceOfLOCKED_LOOT;
-
-  // omit the `TOTAL` & `GUILD` addresses from the ragequit check
   if (
     TOTAL.toHex() != memberAddress.toHex() &&
     GUILD.toHex() != memberAddress.toHex()
   ) {
-    let didFullyRagequit =
-      balanceOfSHARES.equals(BigInt.fromI32(0)) &&
-      balanceOfLOOT.equals(BigInt.fromI32(0)) &&
-      balanceOfLOCKED_LOOT.equals(BigInt.fromI32(0));
+    let membertokenBalanceId = daoAddress
+      .toHex()
+      .concat("-tokenbalances-")
+      .concat(memberAddress.toHex());
 
-    // fully raged quit
-    member.didFullyRagequit = didFullyRagequit;
+    let member = Member.load(memberAddress.toHex());
+    let token = Token.load(tokenAddress.toHex());
+    let tokenBalance = TokenBalance.load(membertokenBalanceId);
+
+    if (member == null) {
+      member = new Member(memberAddress.toHex());
+      member.createdAt = createdAt;
+      member.memberAddress = memberAddress;
+      member.delegateKey = memberAddress;
+      member.isDelegated = false;
+      member.isJailed = false;
+    }
+
+    if (token == null) {
+      token = new Token(tokenAddress.toHex());
+      token.tokenAddress = tokenAddress;
+    }
+
+    if (tokenBalance == null) {
+      tokenBalance = new TokenBalance(membertokenBalanceId);
+      // we give it an initial 0 balance
+      tokenBalance.tokenBalance = BigInt.fromI32(0);
+    }
+
+    /**
+     * get `balanceOf`
+     */
+
+    // get balanceOf member shares
+    let balanceOfSHARES = registry.balanceOf(memberAddress, SHARES);
+    member.shares = balanceOfSHARES;
+
+    // get balanceOf member loot
+    let balanceOfLOOT = registry.balanceOf(memberAddress, LOOT);
+    member.loot = balanceOfLOOT;
+
+    // get balanceOf member locked loot
+    let balanceOfLOCKED_LOOT = registry.balanceOf(memberAddress, LOCKED_LOOT);
+    member.lockedLoot = balanceOfLOCKED_LOOT;
+
+    // omit the `TOTAL` & `GUILD` addresses from the ragequit check
+    if (
+      TOTAL.toHex() != memberAddress.toHex() &&
+      GUILD.toHex() != memberAddress.toHex()
+    ) {
+      let didFullyRagequit =
+        balanceOfSHARES.equals(BigInt.fromI32(0)) &&
+        balanceOfLOOT.equals(BigInt.fromI32(0)) &&
+        balanceOfLOCKED_LOOT.equals(BigInt.fromI32(0));
+
+      // fully raged quit
+      member.didFullyRagequit = didFullyRagequit;
+    }
+
+    tokenBalance.token = tokenAddress.toHex();
+    tokenBalance.member = memberAddress.toHex();
+
+    tokenBalance.tokenBalance = balanceOfSHARES
+      .plus(balanceOfLOOT)
+      .plus(balanceOfLOCKED_LOOT);
+
+    member.save();
+    token.save();
+    tokenBalance.save();
   }
-
-  tokenBalance.token = tokenAddress.toHex();
-  tokenBalance.member = memberAddress.toHex();
-
-  tokenBalance.tokenBalance = balanceOfSHARES
-    .plus(balanceOfLOOT)
-    .plus(balanceOfLOCKED_LOOT);
 
   // get totalShares in the dao
   let balanceOfTotalShares = registry.balanceOf(TOTAL, SHARES);
@@ -88,10 +101,6 @@ function internalTransfer(
 
     dao.save();
   }
-
-  member.save();
-  token.save();
-  tokenBalance.save();
 }
 
 // event NewBalance(address member, address tokenAddr, uint256 amount);
@@ -105,7 +114,12 @@ export function handleNewBalance(event: NewBalance): void {
     ]
   );
 
-  internalTransfer(event.address, event.params.member, event.params.tokenAddr);
+  internalTransfer(
+    event.block.timestamp.toString(),
+    event.address,
+    event.params.member,
+    event.params.tokenAddr
+  );
 }
 
 // event Withdraw(address member, address tokenAddr, uint256 amount);
@@ -119,5 +133,10 @@ export function handleWithdraw(event: Withdraw): void {
     ]
   );
 
-  internalTransfer(event.address, event.params.account, event.params.tokenAddr);
+  internalTransfer(
+    "",
+    event.address,
+    event.params.account,
+    event.params.tokenAddr
+  );
 }
