162f0971146d9672c76150b2b8934ce295b30815
Felipe Forbeck <felipe.forbeck@gmail.com>
2021-04-19 14:27:21 -0300

    removed jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = async () => {
-  return {
-    setupFiles: ["./jest.setup.js"], // https://jestjs.io/pt-BR/docs/configuration#setupfiles-array
-    verbose: true, // https://jestjs.io/docs/configuration#verbose-boolean
-    testTimeout: 25000, // https://jestjs.io/docs/configuration#testtimeout-number
-    rootDir: "./test", // https://jestjs.io/docs/configuration#rootdir-string
-    maxWorkers: 3, // https://jestjs.io/pt-BR/docs/configuration#maxworkers-number--string
-  };
-};
--- test/jest.setup.js
+++
@@ -1,3 +0,0 @@
-const unhandledRejectionPromise = new Promise((resolve) => {
-  process.on("unhandledRejection", resolve);
-});
