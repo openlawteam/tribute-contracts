ae23287e1aca2fc103ca4379b9b32fc626cffb90
jdville03 <jarrel.delottinville@gmail.com>
2021-04-28 11:25:47 -0700

    match arg name

--- contracts/adapters/interfaces/IDistribute.sol
+++ contracts/adapters/interfaces/IDistribute.sol
@@ -32,7 +32,7 @@ interface IDistribute {
     function submitProposal(
         DaoRegistry dao,
         bytes32 proposalId,
-        address unitsHolderAdrr,
+        address unitHolderAdrr,
         address token,
         uint256 amount,
         bytes calldata data
