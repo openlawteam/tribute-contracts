b32d68b654342958f3907d3ba5dcf99b4810f226
sophiacodes <sophiacodes@gmail.com>
2021-06-24 14:19:14 +0100

    restructed subgraph dir to better align with contracts dir

--- subgraph/subgraph-deployer.ts
+++ subgraph/subgraph-deployer.ts
@@ -66,7 +66,7 @@ const getYAML = ({
         eventHandlers:
           - event: DAOCreated(address,string)
             handler: handleDaoCreated
-        file: ./mappings/dao-factory-mapping.ts
+        file: ./mappings/core/dao-factory-mapping.ts
 
 ${couponOnboardingYAML({
   network,
@@ -137,7 +137,7 @@ ${couponOnboardingYAML({
             handler: handleConfigurationUpdated
           - event: AddressConfigurationUpdated(bytes32,address)
             handler: handleAddressConfigurationUpdated
-        file: ./mappings/dao-registry-mapping.ts
+        file: ./mappings/core/dao-registry-mapping.ts
     # ====================================== BankExtension ====================================
     - kind: ethereum/contract
       name: BankExtension
