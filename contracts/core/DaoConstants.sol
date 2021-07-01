fc01123c2e6b466ff3460a45b38863631939bf01
mcchan1 <mcchan1@gmail.com>
2021-07-01 10:34:56 -0400

    init
    setup 1155 Extension, Factory, and Adapter,
    add 1155 to DaoConstants

--- contracts/core/DaoConstants.sol
+++ contracts/core/DaoConstants.sol
@@ -41,12 +41,14 @@ abstract contract DaoConstants {
     bytes32 internal constant CONFIGURATION = keccak256("configuration");
     bytes32 internal constant DISTRIBUTE = keccak256("distribute");
     bytes32 internal constant TRIBUTE_NFT = keccak256("tribute-nft");
+    bytes32 internal constant ERC1155-ADAPT = keccak256("erc1155-adpt");
 
     // Extensions
     bytes32 internal constant BANK = keccak256("bank");
     bytes32 internal constant NFT = keccak256("nft");
     bytes32 internal constant ERC20_EXT = keccak256("erc20-ext");
     bytes32 internal constant EXECUTOR_EXT = keccak256("executor-ext");
+    bytes32 internal constant ERC1155_EXT = keccak256("erc1155-ext");
 
     // Reserved Addresses
     address internal constant GUILD = address(0xdead);
