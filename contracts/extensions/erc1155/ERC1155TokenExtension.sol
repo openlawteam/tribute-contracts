bbe56ad77eb32267f66d8ef96f38de3350620723
mcchan1 <mcchan1@gmail.com>
2021-07-08 10:53:31 -0400

    add 1155 apt to DaoConstants, add 1155 functions

--- contracts/extensions/erc1155/ERC1155TokenExtension.sol
+++ contracts/extensions/erc1155/ERC1155TokenExtension.sol
@@ -8,6 +8,8 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
 /**
 MIT License
 
@@ -40,15 +42,34 @@ contract ERC1155TokenExtension is
 {
     using Address for address payable;
     //LIBRARIES
+    using EnumerableSet for EnumerableSet.UintSet;
+    using EnumerableSet for EnumerableSet.AddressSet;
 
-    bool public initialized = false; //
+    bool public initialized = false; //internally tracks deployment under eip-1167 proxy pattern
     DaoRegistry public dao;
 
-    enum AclFlag {WITHDRAW_1155, COLLECT_1155, INTERNAL_TRANSFER}
+    enum AclFlag {WITHDRAW_NFT, COLLECT_NFT, INTERNAL_TRANSFER}
 
     //EVENTS
+    event CollectedNFT(address nftAddr, uint256 nftTokenId);
+    event TransferredNFT(
+        address nftAddr,
+        uint256 nftTokenId,
+        address oldOwner,
+        address newOwner
+    );
+    event WithdrawnNFT(address nftAddr, uint256 nftTokenId, address toAddress);
 
     //MAPPINGS
+
+    //All the Token IDs that belong to an NFT address stored in the GUILD
+    mapping(address => EnumerableSet.UintSet) private _nfts;
+
+    // The internal owner of record of an NFT that has been transferred to the extension
+    mapping(bytes32 => address) private _ownership;
+
+    //All the NFT addresses collected and stored in the GUILD collection
+    EnumerableSet.AddressSet private _nftAddresses;
 
     //MODIFIERS
     modifier hasExtensionAccess(IExtension extension, AclFlag flag) {
@@ -89,13 +110,15 @@ contract ERC1155TokenExtension is
      */
     function collect(address nftAddr, uint256 nftTokenId)
         external
-        hasExtensionAccess(this, AclFlag.COLLECT_1155)
+        hasExtensionAccess(this, AclFlag.COLLECT_NFT)
     {
         IERC1155 erc1155 = IERC1155(nftAddr);
-        // Move the NFT to the contract address
-        address currentOwner = erc1155.ownerOf(nftTokenId);
+        // Move the NFT to the contract address - unlike 721 no 'ownerOf'
+        uint256 ownerTokenIdBalance = erc1155.balanceOf(msg.sender, nftTokenId);
+        //address currentOwner = erc1155.ownerOf(address(this), nftTokenId);
+
         //If the NFT is already in the NFTExtension, update the ownership if not set already
-        if (currentOwner == address(this)) {
+        if (ownerTokenIdBalance > 0) {
             if (_ownership[getNFTId(nftAddr, nftTokenId)] == address(0x0)) {
                 _saveNft(nftAddr, nftTokenId, GUILD);
 
@@ -103,10 +126,173 @@ contract ERC1155TokenExtension is
             }
             //If the NFT is not in the NFTExtension, we try to transfer from the current owner of the NFT to the extension
         } else {
-            erc1155.safeTransferFrom(currentOwner, address(this), nftTokenId);
+            erc1155.safeTransferFrom(
+                msg.sender,
+                address(this),
+                nftTokenId,
+                1,
+                "0x0"
+            );
             _saveNft(nftAddr, nftTokenId, GUILD);
 
             emit CollectedNFT(nftAddr, nftTokenId);
         }
+    }
+
+    /**
+     * @notice Transfers the NFT token from the extension address to the new owner.
+     * @notice It also updates the internal state to keep track of the all the NFTs collected by the extension.
+     * @notice The caller must have the ACL Flag: WITHDRAW_NFT
+     * @dev Reverts if the NFT is not in ERC721 standard.
+     * @param newOwner The address of the new owner.
+     * @param nftAddr The NFT address that must be in ERC721 standard.
+     * @param nftTokenId The NFT token id.
+     */
+    function withdrawNFT(
+        address newOwner,
+        address nftAddr,
+        uint256 nftTokenId
+    ) public hasExtensionAccess(this, AclFlag.WITHDRAW_NFT) {
+        // Remove the NFT from the contract address to the actual owner
+        IERC1155 erc1155 = IERC1155(nftAddr);
+        erc1155.safeTransferFrom(address(this), newOwner, nftTokenId, 1, "0x0");
+        // Remove the asset from the extension
+        _nfts[nftAddr].remove(nftTokenId);
+        delete _ownership[getNFTId(nftAddr, nftTokenId)];
+
+        // If we dont hold asset from this address anymore, we can remove it
+        if (_nfts[nftAddr].length() == 0) {
+            _nftAddresses.remove(nftAddr);
+        }
+
+        emit WithdrawnNFT(nftAddr, nftTokenId, newOwner);
+    }
+
+    /**
+     * @notice Updates internally the ownership of the NFT.
+     * @notice The caller must have the ACL Flag: INTERNAL_TRANSFER
+     * @dev Reverts if the NFT is not already internally owned in the extension.
+     * @param nftAddr The NFT address.
+     * @param nftTokenId The NFT token id.
+     * @param newOwner The address of the new owner.
+     */
+    function internalTransfer(
+        address nftAddr,
+        uint256 nftTokenId,
+        address newOwner
+    ) public hasExtensionAccess(this, AclFlag.INTERNAL_TRANSFER) {
+        require(newOwner != address(0x0), "new owner is 0");
+        address currentOwner = _ownership[getNFTId(nftAddr, nftTokenId)];
+        require(currentOwner != address(0x0), "nft not found");
+
+        _ownership[getNFTId(nftAddr, nftTokenId)] = newOwner;
+
+        emit TransferredNFT(nftAddr, nftTokenId, currentOwner, newOwner);
+    }
+
+    /**
+     * @notice Gets ID generated from an NFT address and token id (used internally to map ownership).
+     * @param nftAddress The NFT address.
+     * @param tokenId The NFT token id.
+     */
+    function getNFTId(address nftAddress, uint256 tokenId)
+        public
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(nftAddress, tokenId));
+    }
+
+    /**
+     * @notice Returns the total amount of token ids collected for an NFT address.
+     * @param tokenAddr The NFT address.
+     */
+    function nbNFTs(address tokenAddr) public view returns (uint256) {
+        return _nfts[tokenAddr].length();
+    }
+
+    /**
+     * @notice Returns token id associated with an NFT address stored in the GUILD collection at the specified index.
+     * @param tokenAddr The NFT address.
+     * @param index The index to get the token id if it exists.
+     */
+    function getNFT(address tokenAddr, uint256 index)
+        public
+        view
+        returns (uint256)
+    {
+        return _nfts[tokenAddr].at(index);
+    }
+
+    /**
+     * @notice Returns the total amount of NFT addresses collected.
+     */
+    function nbNFTAddresses() external view returns (uint256) {
+        return _nftAddresses.length();
+    }
+
+    /**
+     * @notice Returns NFT address stored in the GUILD collection at the specified index.
+     * @param index The index to get the NFT address if it exists.
+     */
+    function getNFTAddress(uint256 index) external view returns (address) {
+        return _nftAddresses.at(index);
+    }
+
+    /**
+     * @notice Returns owner of NFT that has been transferred to the extension.
+     * @param nftAddress The NFT address.
+     * @param tokenId The NFT token id.
+     */
+    function getNFTOwner(address nftAddress, uint256 tokenId)
+        public
+        view
+        returns (address)
+    {
+        return _ownership[getNFTId(nftAddress, tokenId)];
+    }
+
+    /**
+        @dev Handles the receipt of a single ERC1155 token type. This function is
+        called at the end of a `safeTransferFrom` after the balance has been updated.
+        To accept the transfer, this must return
+        `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))`
+        (i.e. 0xf23a6e61, or its own function selector).
+        @param operator The address which initiated the transfer (i.e. msg.sender)
+        @param from The address which previously owned the token
+        @param id The ID of the token being transferred
+        @param value The amount of tokens being transferred
+        @param data Additional data with no specified format
+        @return `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))` if transfer is allowed
+    */
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external pure override returns (bytes4) {
+        return this.onERC1155Received.selector; // unclear if .selector is correct IERC1155Receiver?
+    }
+
+    // TODO onERC1155BatchReceived
+
+    /**
+     * @notice Helper function to update the extension states for an NFT collected by the extension.
+     * @param nftAddr The NFT address.
+     * @param nftTokenId The token id.
+     * @param owner The address of the owner.
+     */
+    function _saveNft(
+        address nftAddr,
+        uint256 nftTokenId,
+        address owner
+    ) private {
+        // Save the asset
+        _nfts[nftAddr].add(nftTokenId);
+        // set ownership to the GUILD
+        _ownership[getNFTId(nftAddr, nftTokenId)] = owner;
+        // Keep track of the collected assets
+        _nftAddresses.add(nftAddr);
     }
 }
