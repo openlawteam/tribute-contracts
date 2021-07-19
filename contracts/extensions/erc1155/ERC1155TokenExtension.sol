1a8cc276c24d22180f912c5553889aac6bce1f58
mcchan1 <mcchan1@gmail.com>
2021-07-19 12:44:18 -0400

    remove param tags onERC1155Received

--- contracts/extensions/erc1155/ERC1155TokenExtension.sol
+++ contracts/extensions/erc1155/ERC1155TokenExtension.sol
@@ -290,24 +290,14 @@ contract ERC1155TokenExtension is
     }
 
     /**
-        @dev Handles the receipt of a single ERC1155 token type. This function is
-        called at the end of a `safeTransferFrom` after the balance has been updated.
-        To accept the transfer, this must return
-        `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))`
-        (i.e. 0xf23a6e61, or its own function selector).
-        @param operator The address which initiated the transfer (i.e. msg.sender)
-        @param from The address which previously owned the token
-        @param id The ID of the token being transferred
-        @param value The amount of tokens being transferred
-        @param data Additional data with no specified format
-        @return `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))` if transfer is allowed
+        @notice required function from IERC1155 standard to be able to to receive tokens
     */
     function onERC1155Received(
-        address operator,
-        address from,
-        uint256 id,
-        uint256 value,
-        bytes calldata data
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
     ) external pure override returns (bytes4) {
         return this.onERC1155Received.selector; // unclear if .selector is correct IERC1155Receiver?
     }
