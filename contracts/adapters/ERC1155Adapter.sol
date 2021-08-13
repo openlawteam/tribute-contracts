2e28305145b307a854eb91218511f755bc91183a
mcchan1 <michael.chan@consensys.net>
2021-08-13 11:40:55 -0700

    feat(extension): ERC-1155 (#339)
    Added ERC1155 Token Extension, Factory, Adapter, Tests and Docs.
     
    The ERC1155 extension allows the DAO to manage and curate fungible and non-fungible tokens in ERC1155 multi token standard.
    
    Co-authored-by: Felipe Forbeck <felipe.forbeck@gmail.com>

---
+++ contracts/adapters/ERC1155Adapter.sol
@@ -0,0 +1,89 @@
+pragma solidity ^0.8.0;
+
+// SPDX-License-Identifier: MIT
+import "../core/DaoConstants.sol";
+import "../core/DaoRegistry.sol";
+import "../extensions/erc1155/ERC1155TokenExtension.sol";
+import "../guards/AdapterGuard.sol";
+import "../guards/MemberGuard.sol";
+import "./interfaces/IConfiguration.sol";
+import "../adapters/interfaces/IVoting.sol";
+
+/**
+MIT License
+
+Copyright (c) 2020 Openlaw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+contract ERC1155AdapterContract is DaoConstants, AdapterGuard {
+    /**
+     * @notice default fallback function to prevent from sending ether to the contract.
+     */
+    receive() external payable {
+        revert("fallback revert");
+    }
+
+    /**
+     * @notice Collects the NFT from the owner and moves to the ERC1155 Token extension address.
+     * @param dao The DAO address.
+     * @param nftAddr The NFT smart contract address.
+     * @param nftTokenId The NFT token id.
+     * @param amount of the nftTokenId.
+     */
+    function collect(
+        DaoRegistry dao,
+        address nftAddr,
+        uint256 nftTokenId,
+        uint256 amount
+    ) external reentrancyGuard(dao) {
+        ERC1155TokenExtension erc1155 =
+            ERC1155TokenExtension(dao.getExtensionAddress(ERC1155_EXT));
+        erc1155.collect(msg.sender, nftAddr, nftTokenId, amount);
+    }
+
+    /**
+     * @notice Internally transfers the NFT from one owner to a new owner as long as both are active members.
+     * @notice Reverts if the addresses of the owners are not members.
+     * @notice Reverts if the fromOwner does not hold the NFT.
+     * @param dao The DAO address.
+     * @param toOwner The new owner address of the NFT.
+     * @param nftAddr The NFT smart contract address.
+     * @param nftTokenId The NFT token id.
+     * @param amount of the nftTokenId.
+     */
+    function internalTransfer(
+        DaoRegistry dao,
+        address toOwner,
+        address nftAddr,
+        uint256 nftTokenId,
+        uint256 amount
+    ) external reentrancyGuard(dao) {
+        ERC1155TokenExtension erc1155 =
+            ERC1155TokenExtension(dao.getExtensionAddress(ERC1155_EXT));
+        erc1155.internalTransfer(
+            msg.sender,
+            toOwner,
+            nftAddr,
+            nftTokenId,
+            amount
+        );
+    }
+}
---
+++ contracts/test/ERC1155TestToken.sol
@@ -0,0 +1,106 @@
+pragma solidity ^0.8.0;
+
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+
+/**
+MIT License
+
+Copyright (c) 2020 Openlaw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+contract ERC1155TestToken is Context, AccessControlEnumerable, ERC1155 {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    /**
+     * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE`, and `PAUSER_ROLE` to the _creator address
+     */
+    constructor(string memory uri) ERC1155(uri) {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, _msgSender());
+    }
+
+    /**
+     * @dev Creates `amount` new tokens for `to`, of token type `id`.
+     *
+     * See {ERC1155-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `MINTER_ROLE`.
+     */
+    function mint(
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public virtual {
+        require(
+            hasRole(MINTER_ROLE, _msgSender()),
+            "ERC1155License: must have minter role to mint"
+        );
+
+        _mint(to, id, amount, data);
+    }
+
+    /**
+     * @dev xref:ROOT:erc1155.adoc#batch-operations[Batched] variant of {mint}.
+     */
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public virtual {
+        require(
+            hasRole(MINTER_ROLE, _msgSender()),
+            "ERC1155License: must have minter role to mint"
+        );
+
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(AccessControlEnumerable, ERC1155)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override(ERC1155) {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+}
