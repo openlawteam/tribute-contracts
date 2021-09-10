b9d22c079544744ed533165d6d5925cc5dc99a25
david roon <david@roon.me>
2021-09-10 13:53:44 +0200

    full test for lend nft

--- test/adapters/lend-nft.test.js
+++ test/adapters/lend-nft.test.js
@@ -68,12 +68,25 @@ describe("Adapter - LendNFT", () => {
     const pixelNFT = this.testContracts.pixelNFT;
     const nftOwner = accounts[2];
     const lendNFT = this.adapters.lendNFT;
+    const dao = this.dao;
+    const voting = this.adapters.voting;
+    const nftExtension = this.extensions.nft;
+    const bank = this.extensions.bank;
+    const proposalId = getProposalCounter();
+    const proposalId2 = getProposalCounter();
+
+    //mint 2 NFTs
     await pixelNFT.mintPixel(nftOwner, 1, 1, { from: daoOwner });
 
     let pastEvents = await pixelNFT.getPastEvents();
     let { tokenId } = pastEvents[1].returnValues;
-    const dao = this.dao;
-    const proposalId = getProposalCounter();
+
+    await pixelNFT.mintPixel(nftOwner, 2, 2, { from: daoOwner });
+
+    let pastEvents2 = await pixelNFT.getPastEvents();
+    let result = pastEvents2[1].returnValues;
+    let tokenId2 = result.tokenId;
+    //create 2 proposals, one for each NFT
     await lendNFT.submitProposal(
       dao.address,
       proposalId,
@@ -86,35 +99,60 @@ describe("Adapter - LendNFT", () => {
       { from: daoOwner, gasPrice: toBN("0") }
     );
 
-    const voting = this.adapters.voting;
-    const bank = this.extensions.bank;
+    await lendNFT.submitProposal(
+      dao.address,
+      proposalId2,
+      nftOwner,
+      pixelNFT.address,
+      tokenId2,
+      10000,
+      10000, // requested units
+      [],
+      { from: daoOwner, gasPrice: toBN("0") }
+    );
+
+    //approve each NFT to the NFT extension
+
+    await pixelNFT.approve(nftExtension.address, tokenId, { from: nftOwner });
+    await pixelNFT.approve(nftExtension.address, tokenId2, { from: nftOwner });
+
+    //vote them in
 
     await voting.submitVote(dao.address, proposalId, 1, { from: daoOwner });
-    const nftExtension = await dao.getExtensionAddress(NFT);
+    await voting.submitVote(dao.address, proposalId2, 1, { from: daoOwner });
+
     await advanceTime(10000);
-    await pixelNFT.approve(nftExtension, tokenId, { from: nftOwner });
+
+    //process the first proposal
     await lendNFT.processProposal(dao.address, proposalId, { from: daoOwner });
-    let newOwner = await pixelNFT.ownerOf(tokenId);
 
-    expect(newOwner).equal(nftExtension);
-
+    //we see that we have 10K units
     let unitBalance = await bank.balanceOf(nftOwner, UNITS);
     expect(unitBalance.toString()).equal("10000");
 
     await advanceTime(100);
-
+    // after 100 seconds, we take the NFT back
     await expectRevert(
       lendNFT.sendNFTBack(dao.address, proposalId),
       "only the previous owner can withdraw the NFT"
     );
-
     await lendNFT.sendNFTBack(dao.address, proposalId, { from: nftOwner });
-
-    newOwner = await pixelNFT.ownerOf(tokenId);
-
-    expect(newOwner).equal(nftOwner);
 
     unitBalance = await bank.balanceOf(nftOwner, UNITS);
     expect(unitBalance.toString()).equal("100");
+
+    await advanceTime(10000);
+    //process the second proposal
+    await lendNFT.processProposal(dao.address, proposalId2, { from: daoOwner });
+
+    unitBalance = await bank.balanceOf(nftOwner, UNITS);
+    expect(unitBalance.toString()).equal("10100");
+
+    await advanceTime(100);
+
+    //after 100 seconds, get the second NFT back
+    await lendNFT.sendNFTBack(dao.address, proposalId2, { from: nftOwner });
+    unitBalance = await bank.balanceOf(nftOwner, UNITS);
+    expect(unitBalance.toString()).equal("200");
   });
 });
