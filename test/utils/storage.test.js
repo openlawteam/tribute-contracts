742ffdfd7b6a7028368ed769ee28582388dcc27c
david roon <david@roon.me>
2021-10-05 20:27:52 +0200

    remove storage test

--- test/utils/storage.test.js
+++
@@ -1,6 +0,0 @@
-describe("Storage", () => {
-  it("should be possible to create a dao artifacts contract", async () => {
-    const hre = require("hardhat");
-    await hre.storageLayout.export();
-  });
-});
