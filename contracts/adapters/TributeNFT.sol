5607c1fc6453826d2fe52fa7ef0fb25eec5e2fc2
david roon <david@roon.me>
2021-10-08 15:51:00 +0200

    add reimbursable to tribute nft

--- contracts/adapters/TributeNFT.sol
+++ contracts/adapters/TributeNFT.sol
@@ -8,6 +8,7 @@ import "../extensions/erc1155/ERC1155TokenExtension.sol";
 import "../extensions/bank/Bank.sol";
 import "../adapters/interfaces/IVoting.sol";
 import "../guards/AdapterGuard.sol";
+import "./modifiers/Reimbursable.sol";
 
 import "../helpers/DaoHelper.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
@@ -40,7 +41,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 
-contract TributeNFTContract is AdapterGuard, IERC1155Receiver, IERC721Receiver {
+contract TributeNFTContract is AdapterGuard, Reimbursable, IERC1155Receiver, IERC721Receiver {
     using Address for address payable;
 
     struct ProcessProposal {
@@ -101,7 +102,7 @@ contract TributeNFTContract is AdapterGuard, IERC1155Receiver, IERC721Receiver {
         uint256 nftTokenId,
         uint256 requestAmount,
         bytes memory data
-    ) external reentrancyGuard(dao) {
+    ) external reimbursable(dao) {
         require(
             DaoHelper.isNotReservedAddress(applicant),
             "applicant is reserved address"
