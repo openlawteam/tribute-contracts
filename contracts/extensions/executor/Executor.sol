1ddfd3a34c4e909b67983620ee7491dc2285bebd
david roon <david@roon.me>
2022-01-26 13:52:15 +0100

    remove debug code

--- contracts/extensions/executor/Executor.sol
+++ contracts/extensions/executor/Executor.sol
@@ -78,23 +78,6 @@ contract ExecutorExtension is IExtension {
         initialized = true;
     }
 
-    function toAsciiString(address x) internal pure returns (string memory) {
-        bytes memory s = new bytes(40);
-        for (uint256 i = 0; i < 20; i++) {
-            bytes1 b = bytes1(uint8(uint256(uint160(x)) / (2**(8 * (19 - i)))));
-            bytes1 hi = bytes1(uint8(b) / 16);
-            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
-            s[2 * i] = char(hi);
-            s[2 * i + 1] = char(lo);
-        }
-        return string(s);
-    }
-
-    function char(bytes1 b) internal pure returns (bytes1 c) {
-        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
-        else return bytes1(uint8(b) + 0x57);
-    }
-
     /**
      * @dev Delegates the current call to `implementation`.
      *
@@ -120,17 +103,7 @@ contract ExecutorExtension is IExtension {
             daoAddr := mload(add(data, 36))
         }
 
-        require(
-            daoAddr == address(dao),
-            string(
-                abi.encodePacked(
-                    "wrong dao! ",
-                    toAsciiString(address(dao)),
-                    " ",
-                    toAsciiString(daoAddr)
-                )
-            )
-        );
+        require(daoAddr == address(dao), "wrong dao!");
 
         // solhint-disable-next-line no-inline-assembly
         assembly {
