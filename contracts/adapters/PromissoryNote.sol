545c64ee89e45fe61307ee249de0326cdd9d6953
mcchan1 <mcchan1@gmail.com>
2022-03-01 15:15:02 -0500

    init - Create PromissoryNote.sol

---
+++ contracts/adapters/PromissoryNote.sol
@@ -0,0 +1,52 @@
+PromissoryNote.sol
+pragma solidity ^0.8.0;
+
+// SPDX-License-Identifier: MIT
+
+import "../core/DaoRegistry.sol";
+import "../extensions/bank/Bank.sol";
+import "../guards/AdapterGuard.sol";
+import "./modifiers/Reimbursable.sol";
+import "../utils/Signatures.sol";
+import "../helpers/DaoHelper.sol";
+
+/**
+MIT License
+
+Copyright (c) 2020 Openlaw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+contract PromissoryNoteContract is Reimbursable, AdapterGuard, Signatures {
+
+    event LoanCreated(DaoRegistry dao, address Borrower, uint256 amount, uint32 basisPoints, uint256 loanDuration);tt
+
+    struct PromissoryNote {
+        address Borrower;
+        uint256 principalLoanAmount;
+        uint32 basisPoints; 
+        uint256 loanDuration; 
+        uint64 loanStartTime;
+        address nftCollateralContract;
+    }
+
+
+
+}
