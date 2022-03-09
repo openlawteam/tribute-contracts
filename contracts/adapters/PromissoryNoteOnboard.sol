30d0ff13f79b0e3322d83d9241ba7a3eba8896f7
mcchan1 <mcchan1@gmail.com>
2022-03-09 16:45:16 -0500

    change name - basic functions

--- contracts/adapters/PromissoryNoteOnboard.sol
+++ contracts/adapters/PromissoryNoteOnboard.sol
@@ -36,6 +36,10 @@ SOFTWARE.
 
 contract PromissoryNoteContract is Reimbursable, AdapterGuard, Signatures {
 
+    
+    using Address for address payable;
+    using SafeERC20 for IERC20;
+
     event LoanCreated(DaoRegistry dao, address Borrower, uint256 amount, uint32 basisPoints, uint256 loanDuration);
 
     struct PromissoryNote {
@@ -46,30 +50,56 @@ contract PromissoryNoteContract is Reimbursable, AdapterGuard, Signatures {
         uint256 loanDuration; 
         uint64 loanStartTime;
         address nftCollateralContract;
+        //uint64 loadId;
     }
-
+    //
     string public constant COUPON_MESSAGE_TYPE = "Message(address borrower, uint256 principalLoanAmount)";
     bytes32 public constant COUPON_MESSAGE_TYPEHASH =
     keccak256(abi.encodePacked(COUPON_MESSAGE_TYPE));
 
     bytes32 constant SignerAddressConfig = keccak256("promissory-note.signerAddress");
 
+    //encode parameters
+    //bytes32 constant ..
 
+    //map DAO -> loanId ->amount
 
-    event LoanRepaid();
+    // event LoanRepaid(); 
 
-    event LoanForeclosed();
+    // event LoanForeclosed();
 
-    //configureDAO
+    function configureDAO(DaoRegistry dao){
 
-    //hashCouponMessage
-
+    }
     
-    //is NFT deposited
-    //enough funds in Guild
-    //- does the loan start an origination time or upon withdraw?
-    //redeemNote - withdraw funds
-    //repayLoan
-    //forecloseLoan
+    /**
+     * @notice Hashes the provided coupon as an ERC712 hash.
+     * @param dao is the DAO instance to be configured
+     * @param coupon is the coupon to hash
+     */
+    function hashPromissoryNoteMessage(DaoRegistry dao) {
 
+    }
+    
+    /**  
+     * @notice redeems a prommisory note for funds from guildbank
+     * @param dao 
+     */
+    function redeemPromissoryNote(DaoRegistry dao){
+        //approvedPrincipalLoanAmount - 50% of price from WGMI
+        //is NFT deposited
+        // enough funds in Guild
+        // does the loan start an origination time or upon withdraw?
+        //redeemNote - withdraw funds
+    }
+
+ 
+    // User requests a coupon/promissory note
+    // User retrieve value of NFT from database (wmgi api)
+    // User agrees on loan amount based on value of NFT 
+    //User  deposits NFT into guildbank via a startPromissoryNoteAdapter
+    // User receives coupon for 50% of NFT value.
+    // User redeems coupon and receives USDC
+    //     - checks if NFT is deposited
+    // User either return USDC + interest  OR has loan foreclosed. 
 }
