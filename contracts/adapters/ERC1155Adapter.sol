7d078163d8679fe254dd0480b5eedcc7e9d20451
david roon <david@roon.me>
2022-03-15 20:43:48 +0100

    remove unnecessary require

--- contracts/adapters/ERC1155Adapter.sol
+++ contracts/adapters/ERC1155Adapter.sol
@@ -50,10 +50,6 @@ contract ERC1155AdapterContract is AdapterGuard {
         uint256 nftTokenId,
         uint256 amount
     ) external reentrancyGuard(dao) {
-        require(
-            dao.notJailed((DaoHelper.msgSender(dao, msg.sender))),
-            "member is jailed!"
-        );
         ERC1155TokenExtension erc1155 = ERC1155TokenExtension(
             dao.getExtensionAddress(DaoHelper.ERC1155_EXT)
         );
