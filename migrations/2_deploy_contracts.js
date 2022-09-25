195712c49da79b108dd997f001296001575412d4
jdville03 <jarrel.delottinville@gmail.com>
2022-09-25 13:35:51 -0700

    update ganache setup

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -191,36 +191,52 @@ async function deployMainnetDao(deployFunction, network) {
 }
 
 async function deployGanacheDao(deployFunction, network, accounts) {
-  if (!process.env.DAO_NAME) throw Error("Missing env var: DAO_NAME");
-  if (!process.env.COUPON_CREATOR_ADDR)
-    throw Error("Missing env var: COUPON_CREATOR_ADDR");
-  if (!process.env.ERC20_TOKEN_NAME)
-    throw Error("Missing env var: ERC20_TOKEN_NAME");
-  if (!process.env.ERC20_TOKEN_SYMBOL)
-    throw Error("Missing env var: ERC20_TOKEN_SYMBOL");
-  if (!process.env.ERC20_TOKEN_DECIMALS)
-    throw Error("Missing env var: ERC20_TOKEN_DECIMALS");
+  const envVariables = checkEnvVariable(
+    "DAO_NAME",
+    "DAO_OWNER_ADDR",
+    "ERC20_TOKEN_NAME",
+    "ERC20_TOKEN_SYMBOL",
+    "ERC20_TOKEN_DECIMALS",
+    "COUPON_CREATOR_ADDR",
+    "FUND_TARGET_ADDR",
+    "VOTING_PERIOD_SECONDS",
+    "GRACE_PERIOD_SECONDS",
+    "OFFCHAIN_ADMIN_ADDR",
+    "CHUNK_PRICE",
+    "UNITS_PER_CHUNK",
+    "MAX_CHUNKS",
+    "MAX_MEMBERS",
+    "MAX_UNITS",
+    "KYC_PAYMENT_TOKEN",
+    "MANAGER_COUPON_SIGNER_ADDR"
+  );
 
   return await deployDao({
     ...truffleImports,
     deployFunction,
-    unitPrice: toBN(toWei("100", "finney")),
-    nbUnits: toBN("100000"),
-    erc20TokenName: process.env.ERC20_TOKEN_NAME,
-    erc20TokenSymbol: process.env.ERC20_TOKEN_SYMBOL,
-    erc20TokenDecimals: process.env.ERC20_TOKEN_DECIMALS,
-    maxChunks: toBN("100000"),
-    votingPeriod: 120, // 120 secs = 2 mins
-    gracePeriod: 60, // 60 secs = 1 min
+    unitPrice: envVariables.CHUNK_PRICE,
+    nbUnits: envVariables.UNITS_PER_CHUNK,
+    erc20TokenName: envVariables.ERC20_TOKEN_NAME,
+    erc20TokenSymbol: envVariables.ERC20_TOKEN_SYMBOL,
+    erc20TokenDecimals: envVariables.ERC20_TOKEN_DECIMALS,
+    maxChunks: envVariables.MAX_CHUNKS,
+    votingPeriod: envVariables.VOTING_PERIOD_SECONDS,
+    gracePeriod: envVariables.GRACE_PERIOD_SECONDS,
     offchainVoting: true,
     chainId: getNetworkDetails(network).chainId,
     deployTestTokens: true,
-    finalize: false,
+    // deployTestTokens: false,
+    finalize: true,
     maxExternalTokens: 100,
-    couponCreatorAddress: process.env.COUPON_CREATOR_ADDR,
-    fundTargetAddress: process.env.FUND_TARGET_ADDR,
-    daoName: process.env.DAO_NAME,
-    owner: accounts[0],
-    offchainAdmin: "0xedC10CFA90A135C41538325DD57FDB4c7b88faf7",
+    couponCreatorAddress: envVariables.COUPON_CREATOR_ADDR,
+    fundTargetAddress: envVariables.FUND_TARGET_ADDR,
+    managerSignerAddress: envVariables.MANAGER_COUPON_SIGNER_ADDR,
+    daoName: envVariables.DAO_NAME,
+    owner: envVariables.DAO_OWNER_ADDR,
+    offchainAdmin: envVariables.OFFCHAIN_ADMIN_ADDR,
+    maxMembers: envVariables.MAX_MEMBERS,
+    maxUnits: envVariables.MAX_UNITS,
+    wethAddress: "0x2589D644549D794ed63bC3C051D72FDcDC1a875A",
+    kycPaymentToken: envVariables.KYC_PAYMENT_TOKEN,
   });
 }
