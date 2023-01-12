4da544fbf19505ced756dae0841898665ead404a
david roon <david@roon.me>
2023-01-12 14:56:44 +0100

    new coupon burn

---
+++ contracts/adapters/CouponBurn.sol
@@ -0,0 +1,161 @@
+pragma solidity ^0.8.0;
+
+// SPDX-License-Identifier: MIT
+
+import "../core/DaoRegistry.sol";
+import "../extensions/bank/Bank.sol";
+import "../guards/AdapterGuard.sol";
+import "./modifiers/Reimbursable.sol";
+import "../utils/Signatures.sol";
+import "../helpers/DaoHelper.sol";
+
+/**
+MIT License
+
+Copyright (c) 2020 Openlaw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+contract CouponBurnContract is Reimbursable, AdapterGuard, Signatures {
+    struct Coupon {
+        address authorizedMember;
+        uint256 amount;
+        uint256 nonce;
+    }
+
+    using SafeERC20 for IERC20;
+
+    string public constant COUPON_MESSAGE_TYPE =
+        "Message(address authorizedMember,uint256 amount,uint256 nonce)";
+
+    bytes32 public constant COUPON_MESSAGE_TYPEHASH =
+        keccak256(abi.encodePacked(COUPON_MESSAGE_TYPE));
+
+    bytes32 constant SignerAddressConfig =
+        keccak256("coupon-burn.signerAddress");
+
+    bytes32 constant TokenAddrToBurn = keccak256("coupon-burn.tokenAddrToBurn");
+
+    mapping(address => mapping(uint256 => uint256)) private _flags;
+
+    event CouponRedeemed(
+        address daoAddress,
+        uint256 nonce,
+        address authorizedMember,
+        uint256 amount
+    );
+
+    /**
+     * @notice Configures the Adapter with the coupon signer address and token to burn.
+     * @param signerAddress the address of the coupon signer
+     * @param tokenAddrToBurn the address of the token to burn the coupon
+     */
+    function configureDao(
+        DaoRegistry dao,
+        address signerAddress,
+        address tokenAddrToBurn
+    ) external onlyAdapter(dao) {
+        dao.setAddressConfiguration(SignerAddressConfig, signerAddress);
+        dao.setAddressConfiguration(TokenAddrToBurn, tokenAddrToBurn);
+    }
+
+    /**
+     * @notice Hashes the provided coupon as an ERC712 hash.
+     * @param dao is the DAO instance to be configured
+     * @param coupon is the coupon to hash
+     */
+    function hashCouponMessage(DaoRegistry dao, Coupon memory coupon)
+        public
+        view
+        returns (bytes32)
+    {
+        bytes32 message = keccak256(
+            abi.encode(
+                COUPON_MESSAGE_TYPEHASH,
+                coupon.authorizedMember,
+                coupon.amount,
+                coupon.nonce
+            )
+        );
+
+        return hashMessage(dao, address(this), message);
+    }
+
+    /**
+     * @notice Redeems a coupon to add a new member.
+     * @param dao is the DAO instance to be configured
+     * @param authorizedMember is the address that this coupon authorized to become a new member
+     * @param amount is the amount of units that this member will receive
+     * @param nonce is a unique identifier for this coupon request
+     * @param signature is message signature for verification
+     */
+    // function is protected against reentrancy attack with the reentrancyGuard(dao)
+    // slither-disable-next-line reentrancy-benign
+    function redeemCoupon(
+        DaoRegistry dao,
+        address authorizedMember,
+        uint256 amount,
+        uint256 nonce,
+        bytes memory signature
+    ) external reimbursable(dao) {
+        {
+            uint256 currentFlag = _flags[address(dao)][nonce / 256];
+            _flags[address(dao)][nonce / 256] = DaoHelper.setFlag(
+                currentFlag,
+                nonce % 256,
+                true
+            );
+
+            require(
+                DaoHelper.getFlag(currentFlag, nonce % 256) == false,
+                "coupon already redeemed"
+            );
+        }
+
+        Coupon memory coupon = Coupon(authorizedMember, amount, nonce);
+        bytes32 hash = hashCouponMessage(dao, coupon);
+
+        require(
+            SignatureChecker.isValidSignatureNow(
+                dao.getAddressConfiguration(SignerAddressConfig),
+                hash,
+                signature
+            ),
+            "invalid sig"
+        );
+
+        BankExtension bank = BankExtension(
+            dao.getExtensionAddress(DaoHelper.BANK)
+        );
+
+        address tokenAddressToBurn = dao.getAddressConfiguration(
+            TokenAddrToBurn
+        );
+        bank.subtractFromBalance(
+            dao,
+            authorizedMember,
+            tokenAddressToBurn,
+            amount
+        );
+
+        //slither-disable-next-line reentrancy-events
+        emit CouponRedeemed(address(dao), nonce, authorizedMember, amount);
+    }
+}
---
+++ test/adapters/coupon-burn.test.js
@@ -0,0 +1,283 @@
+// Whole-script strict mode syntax
+"use strict";
+
+/**
+MIT License
+
+Copyright (c) 2020 Openlaw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+const { expect } = require("chai");
+const {
+  sha3,
+  toBN,
+  toWei,
+  fromAscii,
+  UNITS,
+  GUILD,
+  ETH_TOKEN,
+} = require("../../utils/contract-util");
+
+const {
+  deployDefaultDao,
+  takeChainSnapshot,
+  revertChainSnapshot,
+  getAccounts,
+  web3,
+} = require("../../utils/hardhat-test-util");
+
+const { checkBalance } = require("../../utils/test-util");
+
+const {
+  SigUtilSigner,
+  getMessageERC712Hash,
+} = require("../../utils/offchain-voting-util");
+
+const signer = {
+  address: "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1",
+  privKey: "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d",
+};
+
+describe("Adapter - Coupon Burn", () => {
+  let accounts, daoOwner;
+  const chainId = 1337;
+
+  before("deploy dao", async () => {
+    accounts = await getAccounts();
+    daoOwner = accounts[0];
+
+    const { dao, adapters, extensions } = await deployDefaultDao({
+      owner: daoOwner,
+      couponCreatorAddress: signer.address,
+    });
+    this.dao = dao;
+    this.adapters = adapters;
+    this.extensions = extensions;
+    this.snapshotId = await takeChainSnapshot();
+  });
+
+  beforeEach(async () => {
+    await revertChainSnapshot(this.snapshotId);
+    this.snapshotId = await takeChainSnapshot();
+  });
+
+  it("should be possible to join a DAO with a valid coupon", async () => {
+    const otherAccount = accounts[2];
+    const signerUtil = SigUtilSigner(signer.privKey);
+
+    const dao = this.dao;
+    const bank = this.extensions.bankExt;
+
+    let signerAddr = await dao.getAddressConfiguration(
+      sha3("coupon-onboarding.signerAddress")
+    );
+    expect(signerAddr).equal(signer.address);
+
+    const couponBurn = this.adapters.couponBurn;
+
+    const couponData = {
+      type: "coupon",
+      authorizedMember: otherAccount,
+      amount: 10,
+      nonce: 1,
+    };
+
+    let jsHash = getMessageERC712Hash(
+      couponData,
+      dao.address,
+      couponBurn.address,
+      chainId
+    );
+    let solHash = await couponBurn.hashCouponMessage(dao.address, couponData);
+    expect(jsHash).equal(solHash);
+
+    var signature = signerUtil(
+      couponData,
+      dao.address,
+      couponBurn.address,
+      chainId
+    );
+
+    var signatureOnboarding = signerUtil(
+      couponData,
+      dao.address,
+      this.adapters.couponOnboarding.address,
+      chainId
+    );
+
+    let balance = await bank.balanceOf(otherAccount, UNITS);
+    expect(balance.toString()).equal("0");
+
+    await this.adapters.couponOnboarding.redeemCoupon(
+      dao.address,
+      otherAccount,
+      10,
+      1,
+      signatureOnboarding
+    );
+
+    balance = await bank.balanceOf(otherAccount, UNITS);
+    expect(balance.toString()).equal("10");
+
+    await couponBurn.redeemCoupon(dao.address, otherAccount, 10, 1, signature);
+
+    const daoOwnerUnits = await bank.balanceOf(daoOwner, UNITS);
+    const otherAccountUnits = await bank.balanceOf(otherAccount, UNITS);
+
+    expect(daoOwnerUnits.toString()).equal("1");
+    expect(otherAccountUnits.toString()).equal("0");
+
+    await checkBalance(bank, GUILD, ETH_TOKEN, toBN("0"));
+  });
+
+  it("should not be possible to join a DAO with mismatched coupon values", async () => {
+    const otherAccount = accounts[2];
+
+    const signerUtil = SigUtilSigner(signer.privKey);
+
+    const dao = this.dao;
+    const bank = this.extensions.bankExt;
+
+    let signerAddr = await dao.getAddressConfiguration(
+      sha3("coupon-burn.signerAddress")
+    );
+    expect(signerAddr).equal(signer.address);
+
+    const couponBurn = this.adapters.couponBurn;
+
+    const couponData = {
+      type: "coupon",
+      authorizedMember: otherAccount,
+      amount: 10,
+      nonce: 1,
+    };
+
+    let jsHash = getMessageERC712Hash(
+      couponData,
+      dao.address,
+      couponBurn.address,
+      1
+    );
+
+    var signature = signerUtil(couponData, dao.address, couponBurn.address, 1);
+
+    const isValid = await couponBurn.isValidSignature(
+      signer.address,
+      jsHash,
+      signature
+    );
+
+    expect(isValid).equal(true);
+
+    let balance = await bank.balanceOf(otherAccount, UNITS);
+    expect(balance.toString()).equal("0");
+
+    await expect(
+      couponBurn.redeemCoupon(dao.address, otherAccount, 100, 1, signature)
+    ).to.be.revertedWith("invalid sig");
+
+    const daoOwnerUnits = await bank.balanceOf(daoOwner, UNITS);
+    const otherAccountUnits = await bank.balanceOf(otherAccount, UNITS);
+
+    expect(daoOwnerUnits.toString()).equal("1");
+    expect(otherAccountUnits.toString()).equal("0");
+
+    await checkBalance(bank, GUILD, ETH_TOKEN, toBN("0"));
+  });
+
+  it("should not be possible to join a DAO with an invalid coupon", async () => {
+    const otherAccount = accounts[2];
+
+    const signerUtil = SigUtilSigner(signer.privKey);
+
+    const dao = this.dao;
+    const bank = this.extensions.bankExt;
+
+    let signerAddr = await dao.getAddressConfiguration(
+      sha3("coupon-burn.signerAddress")
+    );
+    expect(signerAddr).equal(signer.address);
+
+    const couponBurn = this.adapters.couponBurn;
+
+    const couponData = {
+      type: "coupon",
+      authorizedMember: otherAccount,
+      amount: 10,
+      nonce: 1,
+    };
+
+    let jsHash = getMessageERC712Hash(
+      couponData,
+      dao.address,
+      couponBurn.address,
+      1
+    );
+
+    var signature = signerUtil(couponData, dao.address, couponBurn.address, 1);
+
+    const isValid = await couponBurn.isValidSignature(
+      signer.address,
+      jsHash,
+      signature
+    );
+
+    expect(isValid).equal(true);
+    let balance = await bank.balanceOf(otherAccount, UNITS);
+    expect(balance.toString()).equal("0");
+
+    await expect(
+      couponBurn.redeemCoupon(dao.address, daoOwner, 10, 1, signature)
+    ).to.be.revertedWith("invalid sig");
+
+    const daoOwnerUnits = await bank.balanceOf(daoOwner, UNITS);
+    const otherAccountUnits = await bank.balanceOf(otherAccount, UNITS);
+
+    expect(daoOwnerUnits.toString()).equal("1");
+    expect(otherAccountUnits.toString()).equal("0");
+
+    await checkBalance(bank, GUILD, ETH_TOKEN, toBN("0"));
+  });
+
+  it("should not be possible to send ETH to the adapter via receive function", async () => {
+    const adapter = this.adapters.couponBurn;
+    await expect(
+      web3.eth.sendTransaction({
+        to: adapter.address,
+        from: daoOwner,
+        gasPrice: toBN("0"),
+        value: toWei("1"),
+      })
+    ).to.be.revertedWith("revert");
+  });
+
+  it("should not be possible to send ETH to the adapter via fallback function", async () => {
+    const adapter = this.adapters.couponBurn;
+    await expect(
+      web3.eth.sendTransaction({
+        to: adapter.address,
+        from: daoOwner,
+        gasPrice: toBN("0"),
+        value: toWei("1"),
+        data: fromAscii("should go to fallback func"),
+      })
+    ).to.be.revertedWith("revert");
+  });
+});
