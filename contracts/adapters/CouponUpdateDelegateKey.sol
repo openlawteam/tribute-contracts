a4caad548879a5bc09a222a4d8e63db45be4e967
David Roon <david.roon@gmail.com>
2024-10-15 17:55:47 +0200

    update date

--- contracts/adapters/CouponUpdateDelegateKey.sol
+++ contracts/adapters/CouponUpdateDelegateKey.sol
@@ -12,7 +12,7 @@ import "../helpers/DaoHelper.sol";
 /**
 MIT License
 
-Copyright (c) 2020 Openlaw
+Copyright (c) 2024 Openlaw
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
